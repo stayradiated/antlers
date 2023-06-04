6c96305ec402cda30cb6e48966d450dec913fc38
George Czabania <1140167+stayradiated@users.noreply.github.com>
2023-06-04 21:14:08 +0200

    feat: specify sizes when using srcset

--- apps/web/src/components/image/utils.ts
+++ apps/web/src/components/image/utils.ts
@@ -4,12 +4,15 @@ import type { Image } from "~/db.js";
 type RenderableImage = {
   src: string;
   srcSet: string[];
+  sizes: string;
   aspectRatio: number;
   width: number;
   height: number;
 };
 
-const defaultWidthList = [16, 320, 640, 960, 1280, 1920, 2500];
+const defaultWidthList = [
+  16, 320, 640, 672, 1280, 1344, 1280, 1920, 2016, 2500,
+];
 
 type Options = Pick<Image, "id" | "width" | "height">;
 
@@ -24,9 +27,12 @@ const buildRenderableImage = (options: Options): RenderableImage => {
   });
   const maxWidthUrl = urls.slice(-1)[0]!;
 
+  const sizes = "(max-width: 692px) 100w, 672px";
+
   return {
     src: maxWidthUrl,
     srcSet,
+    sizes,
     aspectRatio,
     width,
     height,
